b3aa5d525e17c35c02f3649e984c03a3e8445a7e
MelleRx <kukulidi@yandex.ru>
2020-10-15 19:29:30 +0300

    client changes

--- lab_2/client.cpp
+++ lab_2/client.cpp
@@ -3,12 +3,14 @@
 #include <netinet/in.h>
 #include <iostream>
 
+char message[] = "Hello there!\n";
+char buf[sizeof(message)];
+
+
 int main() {
     int sock;
     struct sockaddr_in addr;
-    struct hostent* hostinfo;
 
-    port = atoi(80);
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("socket");
@@ -16,8 +18,8 @@ int main() {
     }
 
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(3425);
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
     if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         perror("connect");
@@ -27,6 +29,7 @@ int main() {
     send(sock, message, sizeof(message), 0);
     recv(sock, buf, sizeof(message), 0);
 
+    printf(buf);
     close(sock);
 
     return 0;
