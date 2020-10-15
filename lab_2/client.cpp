15087c347691ecb2d1af58dc2a5db0daf967362d
MelleRx <kukulidi@yandex.ru>
2020-10-15 21:12:08 +0300

    client test(4)

--- lab_2/client.cpp
+++ lab_2/client.cpp
@@ -24,7 +24,7 @@ int main(int argc, char **argv) {
 
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);;
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
     if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         perror("connect");
