55764f84c34df27b83ba3bcf98b1023c16c7bb6d
MelleRx <kukulidi@yandex.ru>
2020-10-15 03:05:30 +0300

    normal fix(part 2)

--- lab_2/server.cpp
+++ lab_2/server.cpp
@@ -7,6 +7,7 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <stdio.h>
+#include <iostream>
 
 int main() {
     int sock, listener;
@@ -49,7 +50,7 @@ int main() {
             // reading data from socket(socket, buf pointer, len of buf, combination of bit flags)
             bytes_read = recv(sock, buf, 1024, 0); // if flags = 0 -> delete data from the socket
             if (bytes_read <= 0) break;
-            printf("Oleg molodec");
+            std::cout << "Oleg molodec";
             send(sock, buf, bytes_read, 0); // sending data(socket, buf pointer, len of buf, combination of bit flags)
         }
         close(sock); // closing a socket
